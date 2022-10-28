50b29e1d3141f368d688c3953f823c0ff3049c00
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-28 10:38:18 +0530

    first commit

---
+++ inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace inheritance
+{
+   
+        class Vehicle
+        {
+            public string name = "tesla";
+            public void honk()
+            {
+                Console.WriteLine("elon,musk");
+
+            }
+
+        }
+        class Car : Vehicle
+        {
+            public string model1Name = "tesla1";
+
+        }
+        class program
+        {
+            static void Main(string[] args)
+            {
+                Car myCar = new Car();
+                myCar.honk();
+                Console.WriteLine(myCar.name + " " + myCar.model1Name);
+            }
+        }
+    }
